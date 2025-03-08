989483e4269c6e6372b39105559348829b743a9f
Stephen882-pixel <ondeyostephen0@gmail.com>
2025-03-08 21:58:19 +0300

    urls

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ urlpatterns  = [
     path('login/',LoginView.as_view(),name='login'),
     path('policies/',PolicyListCreateView.as_view(),name='policy-list-create'),
     path('claims/',ClaimListCreateView.as_view(),name='claim-list-create'),
-    path('claims/<int;pk>/',ClaimDetailView.as_view(),name='claim-detail')
+    path('claims/<int:pk>/',ClaimDetailView.as_view(),name='claim-detail')
 ]
