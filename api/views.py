f8d6081c98366c2cb05b43e7925f1d19d5819cea
Stephen882-pixel <ondeyostephen0@gmail.com>
2025-03-08 11:41:57 +0300

    created views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets,permissions
+from rest_framework.response import Response
+from .models import User,Payment,Policy,Auditlog,Claim
+from .serializers import UserSerializer,PaymentSerializer,PolicySerializer,AuditLogSerializer,ClaimSerializer
 
-# Create your views here.
+
+
+# Policy viewset
+class PolicyViewSet(viewsets.ModelViewSet):
+    queryset = Policy.objects.all()
+    serializer_class = PolicySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Claim viewset
+class ClaimViewSet(viewsets.ModelViewSet):
+    queryset = Claim.objects.all()
+    serializer_class = ClaimSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+# Payment viewwset
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Audit log viewset
+class AuditLogViewSet(viewsets.ModelViewSet):
+    queryset = Auditlog.objects.all()
+    serializer_class = AuditLogSerializer
+    permission_classes = [permissions.IsAdminUser]
+    
